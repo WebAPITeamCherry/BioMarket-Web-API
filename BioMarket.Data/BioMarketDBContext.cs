6e33011728e9f2826b8687c8a6b67f4ed9802da8
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-09-17 15:47:28 +0300

    Initial Commit

---
+++ BioMarket.Data/BioMarketDBContext.cs
@@ -0,0 +1,46 @@
+﻿namespace BioMarket.Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using BioMarket.Data.Migrations;
+    using BioMarket.Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class BioMarketDBContext : IdentityDbContext<Account>, IBioMarketDBContext
+    {
+        public BioMarketDBContext()
+            : base("BioMarketConnection", throwIfV1Schema: false)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BioMarketDBContext, Configuration>());
+        }
+
+        public IDbSet<Product> Products { get; set; }
+
+        public IDbSet<Offer> Offers { get; set; }
+
+        public IDbSet<Farm> Farms { get; set; }
+
+        public IDbSet<Client> Clients { get; set; }
+
+        public static BioMarketDBContext Create()
+        {
+            return new BioMarketDBContext();
+        }
+
+        public new IDbSet<T> Set<T>() where T : class
+        {
+            return base.Set<T>();
+        }
+
+        public new void SaveChanges()
+        {
+            base.SaveChanges();
+        }
+
+        public new void Dispose()
+        {
+            base.Dispose();
+        }
+    }
+}
---
+++ BioMarket.Models/Account.cs
@@ -0,0 +1,17 @@
+﻿namespace BioMarket.Models
+{
+    using System;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class Account : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<Account> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);            
+            return userIdentity;
+        }
+    }
+}
